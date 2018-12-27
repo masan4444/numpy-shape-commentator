ec996516832e0c80be0070509befe43d5f4c2722
masan4444 <unixer.pro@MacBookPro2016.local>
2018-12-27 18:30:05 +0900

    initial commit

---
+++ spec/numpy-shape-commentator-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NumpyShapeCommentatorView from '../lib/numpy-shape-commentator-view';
+
+describe('NumpyShapeCommentatorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
