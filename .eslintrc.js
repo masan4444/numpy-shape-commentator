53cfadb2733ee55b61257b1ff013058fc59413a9
masan4444 <unixer.pro@MacBookPro2016.local>
2019-02-01 00:23:03 +0900

    [update] add eslint plugin for jasmine

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,21 @@
 module.exports = {
-    "extends": ["airbnb", "plugin:import/errors", "plugin:import/warnings"],
+    "extends": [
+      "airbnb",
+      "plugin:import/errors",
+      "plugin:import/warnings",
+      "plugin:jasmine/recommended"
+    ],
+    "plugins": [
+      "import",
+      "jasmine"
+    ],
     "rules": {
         'no-plusplus': [2, { allowForLoopAfterthoughts: true }]
     },
     "env": {
         "browser": true,
-        "es6": true
+        "es6": true,
+        "jasmine": true
     },
     "settings": {
         "import/core-modules": ["atom"]
